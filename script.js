c57c7cb51e52445e350cd0d597e6ffa337869a64
jaketverberg <54115094+jaketverberg@users.noreply.github.com>
2021-09-17 12:33:16 -0500

    All Javascript working with controls. Functional.

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 const DEFAULT_COLOR = '#333333';
 const DEFAULT_GRID = 16;
-const GRID_SIZE_TEXT = document.createElement('p')
+const BUTTON = document.querySelector('#button')
+const COLOR_PICKER = document.querySelector('#colorpicker')
+const SLIDER_SELECTOR = document.querySelector('#sizeSlider')
 
+let gridSizeText = document.querySelector('#sliderNumbers')
 let currentColor = DEFAULT_COLOR;
 let currentGrid = DEFAULT_GRID;
+gridSizeText.textContent = `${currentGrid} X ${currentGrid}`;
 
-grid.style.gridTemplateColumns = `repeat ${currentGrid}, 1fr`;
-grid.style.gridTemplateRows = `repeat ${currentGrid}, 1fr`;
-
-function changeColor () {
-    let newColor = document.getElementsById('this');
-    currentColor = newColor;
+function changeColor (e) {
+    e.target.style.backgroundColor = currentColor;
 }
 
-const buttons = document.querySelectorAll('.grid');
-buttons.forEach((button) => {
-  button.addEventListener('hover', () => {
-    playerGuess = `${button.id}`;
-    playRound();
-  });
-});
+function drawGrid (size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div')
+        gridElement.classList.add('div');
+        gridElement.addEventListener('mouseover', changeColor)
+        grid.appendChild(gridElement)
+      }
+}
+
+function clearGridBackground () {
+    grid.innerHTML = '';
+    drawGrid(currentGrid);
+}
+
+function changeGrid() {
+    currentGrid = SLIDER_SELECTOR.value;
+    gridSizeText.textContent = `${currentGrid} X ${currentGrid}`
+    clearGridBackground ();
+}
+
+SLIDER_SELECTOR.onchange = (e) => changeGrid();
+COLOR_PICKER.onchange = (e) => currentColor = e.target.value;
+BUTTON.onclick = () => clearGridBackground();
+
+drawGrid (currentGrid);
